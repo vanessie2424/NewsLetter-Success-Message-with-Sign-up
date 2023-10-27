c8ade05eec771e170afc83dcea513b8481830445
Anazodo Vanessa <anazodovanessa19@gmail.com>
2023-10-27 07:16:54 +0100

    newsletter

--- newsletter_signup/src/App.jsx
+++ newsletter_signup/src/App.jsx
@@ -1,4 +1,4 @@
-import {Formik, useFormik} from 'formik'
+import {useFormik} from 'formik'
 import { signUpSchema } from './Schemas'
 import { useState } from 'react'
 import Thanks from './components/thanks'
@@ -40,21 +40,21 @@ import Thanks from './components/thanks'
   
   return (
     <div className=" flex justify-center items-center min-h-screen bg-dark-grey font-roboto max-md:w-full ">
-          { !showThankMessage ? (<div className='flex justify-center items-center p-5 bg-white-own px-2 rounded-3xl max-md:flex-col-reverse'>
-        <div className='px-6 text-charcoal-grey'>
-           <h1 className='pb-6 text-charcoal-grey text-[40px] font-bold'>Stay updated !</h1>
+          { !showThankMessage ? (<div className='flex justify-center items-center p-5 bg-white-own px-2 rounded-3xl max-md:flex-col-reverse max-md:pt-0'>
+        <div className='px-6 text-charcoal-grey max-md:px-8'>
+           <h1 className='pb-6 text-charcoal-grey text-[37px] font-bold max-md:pt-6'>Stay updated !</h1>
            <p className='font-semibold pb-5'>Join 60,000+ product managers receiving monthly updates on: </p>
            <div className='flex items-center gap-3 pb-4 font-normal'>
             <img src="images/icon-list.svg" alt="icon-list" />
-            <p>Product discovery and building what matters</p>
+            <p className=' max-md:text-sm'>Product discovery and building what matters</p>
            </div>
            <div className='flex items-center gap-3 pb-5 font-normal'>
             <img src="images/icon-list.svg" alt="icon-list" />
-            <p>Measuring to ensure updates are a success</p>
+            <p className=' max-md:text-sm'>Measuring to ensure updates are a success</p>
            </div>
            <div className='flex items-center gap-3 pb-5 font-normal'>
             <img src="images/icon-list.svg" alt="icon-list" />
-            <p> And much more! </p>
+            <p className=' max-md:text-sm'> And much more! </p>
            </div>
 
            <div className='flex items-center justify-between py-5'>
@@ -63,10 +63,10 @@ import Thanks from './components/thanks'
            </div> 
            <div>
              <form className='flex justify-center items-center flex-col gap-7' onSubmit={handleSubmit}>
-               <input style={formStyle} className=' bg-white-own border-solid border-charcoal-grey border-[1.3px] rounded-md border-spacing-1.5 w-[350px] h-10 pl-3 text-charcoal-grey' type="email" placeholder={!touched.email ? 'email@company.com' : null} name='email'
+               <input style={formStyle} className=' bg-white-own border-solid border-charcoal-grey border-[1.3px] rounded-md border-spacing-1.5 w-[350px] h-10 pl-3 text-charcoal-grey max-md:w-[320px]' type="email" placeholder={!touched.email ? 'email@company.com' : null} name='email'
                   value={values.email} onBlur={handleBlur} onChange={handleChange}/>
                   {console.log(values)}
-                <button className=' bg-charcoal-grey w-[350px] h-12 text-white-own font-semibold rounded-md hover:bg-tomato' type='submit'>Subscribe to monthly newsletter</button>
+                <button className=' bg-charcoal-grey w-[350px] h-12 text-white-own font-semibold rounded-md hover:bg-tomato max-md:w-[320px] max-md:mt-5' type='submit'>Subscribe to monthly newsletter</button>
 
              </form>
             </div>  
